098ad9fde5687c90d78421767601667466571fb6
DomLJ <dominik34@o2.pl>
2020-12-04 20:02:53 +0100

    Create basic generation view

---
+++ src/generation/GenerationCharacterList.tsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { WorkSlot } from './GenerationView'
+
+interface GenerationCharacterListProps {
+  charSet: WorkSlot[]
+  handleDimentionChange: (event: React.ChangeEvent<HTMLInputElement>, width: boolean, char: WorkSlot) => void
+  defaultWidth: number
+  defaultHeight: number
+}
+
+class GenerationCharacterList extends Component<GenerationCharacterListProps, {}> {
+  constructor(props: GenerationCharacterListProps) {
+    super(props)
+
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    const charList = this.props.charSet.map(char => 
+      <div key={char.character}>
+        <p>{char.character}</p>
+        <input 
+          type='number' 
+          value={char.width ?? this.props.defaultWidth} 
+          onChange={(event) => this.props.handleDimentionChange(event, true, char)}
+        />
+
+        <input
+          type='number' 
+          value={char.height ?? this.props.defaultHeight} 
+          onChange={(event) => this.props.handleDimentionChange(event, false, char)}
+        />
+      </div>
+    )
+
+    return (
+      <div>
+        {charList}
+      </div>
+    )
+  }
+}
+
+export default GenerationCharacterList
---
+++ src/generation/GenerationView.tsx
@@ -0,0 +1,118 @@
+import React, { Component } from 'react'
+import { bind } from 'helpful-decorators'
+import GenerationCharacterList from './GenerationCharacterList'
+
+export interface WorkSlot {
+  character: string
+  width?: number 
+  height?: number 
+}
+
+interface GenerationViewState {
+  charSet: WorkSlot[]
+  defaultWidth: number
+  defaultHeight: number
+}
+
+class GenerationView extends Component<{}, GenerationViewState> {
+  constructor(props: {}) {
+    super(props)
+
+    this.state = {
+      charSet: this.createCharArray(),
+      defaultWidth: 20,
+      defaultHeight: 20
+    }
+  }
+
+  createCharArray(): WorkSlot[] {
+    return Array.from('AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz.?!,:', (character: string) => ({character: character}))
+  }
+
+  @bind
+  handleCharSetInput(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    const newCharArray = event.target.value.split("")
+    const isUniqueCharSet = new Set(newCharArray).size === newCharArray.length
+
+    const newCharSet: WorkSlot[] = newCharArray.map(character => {
+      return this.state.charSet.find(oldChar => oldChar.character === character) ?? {character: character}
+    })
+
+    event.preventDefault()
+
+    if (isUniqueCharSet) {
+      this.setState({
+        charSet: newCharSet
+      })
+    }
+  }
+
+  get charString(): string {
+    return this.state.charSet.map(char => char.character).join("")
+  }
+
+  @bind
+  handleDefaultDimentionChange(event: React.ChangeEvent<HTMLInputElement>, width: boolean) {
+    const dimention = width ? 'defaultWidth' : 'defaultHeight'
+
+    event.preventDefault()
+
+    this.setState(prevState => ({
+      ...prevState,
+      [dimention]: event.target.value
+    }))
+  }
+  
+  @bind
+  handleDimentionChange(event: React.ChangeEvent<HTMLInputElement>, width: boolean, char: WorkSlot) {
+    const dimention = width ? 'width' : 'height'
+
+    const newCharSet: WorkSlot[] = this.state.charSet.map(character => character === char 
+      ? {
+        ...character,
+        [dimention] : event.target.value
+      }
+      : character
+    )
+
+    this.setState({
+      charSet: newCharSet
+    })
+  }
+
+  render() {
+    return (
+      <div className='generation-view'>
+        <textarea 
+          onChange={this.handleCharSetInput}
+          value={this.charString}
+        />
+
+        <label>width</label>
+        <input 
+          type='number' 
+          onChange={event => this.handleDefaultDimentionChange(event, true)} 
+          value={this.state.defaultWidth}
+        />
+        <label>height</label>
+        <input 
+          type='number' 
+          onChange={event => this.handleDefaultDimentionChange(event, false)} 
+          value={this.state.defaultHeight}
+        />
+
+        <GenerationCharacterList 
+          charSet={this.state.charSet} 
+          defaultHeight={this.state.defaultHeight} 
+          defaultWidth={this.state.defaultWidth} 
+          handleDimentionChange={this.handleDimentionChange}
+        />
+
+        
+      </div>
+    )
+  }
+
+}
+
+export default GenerationView
