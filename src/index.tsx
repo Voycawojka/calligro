e2d5077cbc57d65a27c2a344aa78c2f087b6f7f0
Voycawojka <filip.artur.kowalski@gmail.com>
2020-12-05 00:01:55 +0100

    Remove unnecessary imports from index

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Template from './generation/template/Template';
-import { downloadTemplate } from './generation/template/download';
 
 ReactDOM.render(
   <React.StrictMode>
