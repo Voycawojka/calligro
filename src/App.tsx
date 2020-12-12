6be809c37afcaf12a9801aec744092a43cea3919
codefactor-io <support@codefactor.io>
2020-12-12 17:10:19 +0000

    [CodeFactor] Apply fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import FontCreationView from './ui/generation/fontCreationView/FontCreationView'
 import GenerationView from './ui/generation/generationView/GenerationView'
-import { HashRouter, Route, Switch } from 'react-router-dom' 
+import { HashRouter, Route, Switch } from 'react-router-dom'
 import Header from './ui/generation/header/Header'
 import Footer from './ui/generation/footer/Footer'
 import Policy from './ui/generation/policy/Policy'
