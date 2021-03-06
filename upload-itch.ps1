d32d93fd866bd4258ca4e9d6325bd76eba580364
Voycawojka <filip.artur.kowalski@gmail.com>
2021-03-06 21:32:59 +0100

    Use proper version when uploading to itch

--- upload-itch.ps1
+++ upload-itch.ps1
@@ -5,7 +5,7 @@
 npm run electron:build
 
 # Upload to itch
-butler push electron_build\calligro-win32-x64 voycawojka/calligro win-64
-butler push electron_build\calligro-linux-x64 voycawojka/calligro linux
+butler push electron_build\calligro-win32-x64 voycawojka/calligro:win-64 --userversion-file version.txt
+butler push electron_build\calligro-linux-x64 voycawojka/calligro:linux --userversion-file version.txt
 
 Write-Host "Upload done"
